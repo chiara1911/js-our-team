91665bec8eec041aae30d6f85480e0817bdb03ac
chiara1911 <chiaracesari89@gmail.com>
2023-10-23 15:39:34 +0200

    add ourTeam array

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// 1. MILESTONE:
+// 
+// 2. MILESTONE:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//  3. MILESTONE :
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// Trasformare la stringa foto in una immagine effettiva
+
+
+// ### BONUS :
+// - Organizzare i singoli membri in card/schede
+// - Aggiungere un nuovo membro al team
+
+// Creare l’array di oggetti con le informazioni fornite.
+
+const OurTeam =[
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        picture:'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        picture:'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        picture:'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        picture:'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        picture:'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        picture:'barbara-ramos-graphic-designer.jpg'
+    }
+]
+
+console.log(OurTeam)
