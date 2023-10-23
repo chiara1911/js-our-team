6875f76b9a6d1e5233369f1e1944fc875f0d249a
chiara1911 <chiaracesari89@gmail.com>
2023-10-23 16:44:10 +0200

    ticket

--- js/script.js
+++ js/script.js
@@ -17,37 +17,72 @@
 
 // Creare l’array di oggetti con le informazioni fornite.
 
-const OurTeam =[
+"use string";
+
+/* <div class="col-4">
+    <div class="box-card">
+        <img src="" alt="">  <!-- picture-->
+            <h4></h4>  <!-- nome -->
+            <h5></h5>  <!--  role -->
+    </div>
+</div> */
+
+// creo array contenente gli elementi del team
+const ourTeam = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        picture:'wayne-barnett-founder-ceo.jpg'
+        picture: 'wayne-barnett-founder-ceo.jpg'
     },
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
-        picture:'angela-caroll-chief-editor.jpg'
+        picture: 'angela-caroll-chief-editor.jpg'
     },
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        picture:'walter-gordon-office-manager.jpg'
+        picture: 'walter-gordon-office-manager.jpg'
     },
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        picture:'angela-lopez-social-media-manager.jpg'
+        picture: 'angela-lopez-social-media-manager.jpg'
     },
     {
         name: 'Scott Estrada',
         role: 'Developer',
-        picture:'scott-estrada-developer.jpg'
+        picture: 'scott-estrada-developer.jpg'
     },
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        picture:'barbara-ramos-graphic-designer.jpg'
+        picture: 'barbara-ramos-graphic-designer.jpg'
     }
-]
+];
+console.log(ourTeam);
+const rowEl = document.querySelector('row');
 
-console.log(OurTeam)
+
+// ciclo per stampare gli elementi dell'array
+for(let i = 0; i < ourTeam.length; i++){
+    printBox(ourTeam[i]);
+   
+};
+// creo funzione per mettere le box dentro a html
+function printBox(team){
+    const col = document.createElement("div");
+    col.classList.add("col-4");
+    const template =` 
+        <div class="box-card">
+            <img src="/img/${team.picture}" alt="">  
+            <h4>${team.role}</h4>  
+            <h5>${team.name}</h5>          
+         </div> `;    
+         console.log(team.role);
+         console.log(team.picture);
+         console.log(team.name);
+    col.innerHTML = template;   
+    rowEl.append(col);
+  
+};
